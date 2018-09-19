675cdf4d1baca1820f900f1a25322ed6676bd710
Lars Kanis <kanis@comcard.de>
2018-09-19 17:30:59 +0200

    Add a sample Queue implementation as test case

--- test/eventbox_test.rb
+++ test/eventbox_test.rb
@@ -832,4 +832,54 @@ class EventboxTest < Minitest::Test
     end
     assert_equal "abcdefgdefhi", res
   end
+
+  class TestQueue < Eventbox
+    async_call def init
+      @que = []
+      @waiting = []
+    end
+
+    async_call def enq(value)
+      @que << value
+      if w=@waiting.shift
+        w.yield @que.shift
+      end
+    end
+
+    yield_call def deq(result)
+      if @que.empty?
+        @waiting << result
+      else
+        result.yield @que.shift
+      end
+    end
+  end
+
+  def test_queue
+    q = TestQueue.new
+
+    # Start one thread which enqueues values
+    Thread.new do
+      1000.times do |i|
+        q.enq i
+      end
+      q.enq nil
+      q.enq nil
+    end
+
+    # Start two thread which fetch values concurrently
+    l1, l2 = 2.times.map do
+      Thread.new do
+        l = []
+        while v=q.deq
+          l << v
+        end
+        l
+      end
+    end.map(&:value)
+
+    assert_operator 1..999, :===, l1.size
+    assert_operator 1..999, :===, l2.size
+    assert_equal 1000.times.to_a, (l1 + l2).sort
+  end
 end
