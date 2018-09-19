cc3e016afd72ab42323fe258abb33d4b6e9b2d61
Lars Kanis <kanis@comcard.de>
2018-09-19 15:34:52 +0200

    Change event loop callbacks to instance variable
    This is better compatible to the MRI garbage collector.

--- lib/eventbox/event_loop.rb
+++ lib/eventbox/event_loop.rb
@@ -38,48 +38,52 @@ class Eventbox
     # The event loop processes the input queue by executing the enqueued method calls.
     # It can be stopped by #shutdown .
     def run_event_loop
-      latest_call = nil
       loop do
         call = @input_queue.deq
-        latest_call = call
+        @latest_call = call
+        break if process_one_call(call)
+        @latest_call = nil
+      end
+    end
 
-        cb_handler = proc do |*cbargs, &cbresult|
-          cbargs = sanity_after_queue(cbargs)
-          case latest_call
-          when YieldCall, SyncCall
-            latest_call.answer_queue << Callback.new(call.box, cbargs, cbresult, call.block)
-          else
-            raise(InvalidAccess, "closure defined by `#{call.name}' was yielded by `#{latest_call.name}', which must a sync_call or yield_call")
-          end
-        end
-
-        case call
-        when SyncCall
-          res = call.box.send("__#{call.name}__", *sanity_after_queue(call.args), &cb_handler)
-          res = sanity_before_queue(res)
-          call.answer_queue << res
-        when YieldCall
-          result = nil
-          call.box.send("__#{call.name}__", *sanity_after_queue(call.args), proc do |*resu|
-            raise MultipleResults, "received multiple results for method `#{call.name}'" if result
-            result = resu
-            resu = return_args(resu)
-            resu = sanity_before_queue(resu)
-            call.answer_queue << resu
-          end, &cb_handler)
-        when AsyncCall
-          call.box.send("__#{call.name}__", *sanity_after_queue(call.args), &cb_handler)
-        when CallbackResult
-          cbres = sanity_after_queue(call.res)
-          call.cbresult.yield(cbres)
-        when ThreadFinished
-          @action_threads.delete(call.thread) or raise(ArgumentError, "unknown thread has finished")
-        when :shutdown
-          break
+    def process_one_call(call)
+      cb_handler = proc do |*cbargs, &cbresult|
+        cbargs = sanity_after_queue(cbargs)
+        case @latest_call
+        when YieldCall, SyncCall
+          @latest_call.answer_queue << Callback.new(call.box, cbargs, cbresult, call.block)
         else
-          raise ArgumentError, "invalid call type #{call.inspect}"
+          raise(InvalidAccess, "closure defined by `#{call.name}' was yielded by `#{@latest_call.name}', which must a sync_call or yield_call")
         end
       end
+
+      case call
+      when SyncCall
+        res = call.box.send("__#{call.name}__", *sanity_after_queue(call.args), &cb_handler)
+        res = sanity_before_queue(res)
+        call.answer_queue << res
+      when YieldCall
+        result = nil
+        call.box.send("__#{call.name}__", *sanity_after_queue(call.args), proc do |*resu|
+          raise MultipleResults, "received multiple results for method `#{call.name}'" if result
+          result = resu
+          resu = return_args(resu)
+          resu = sanity_before_queue(resu)
+          call.answer_queue << resu
+        end, &cb_handler)
+      when AsyncCall
+        call.box.send("__#{call.name}__", *sanity_after_queue(call.args), &cb_handler)
+      when CallbackResult
+        cbres = sanity_after_queue(call.res)
+        call.cbresult.yield(cbres)
+      when ThreadFinished
+        @action_threads.delete(call.thread) or raise(ArgumentError, "unknown thread has finished")
+      when :shutdown
+        return true
+      else
+        raise ArgumentError, "invalid call type #{call.inspect}"
+      end
+      false
     end
 
     def start_action(meth, args)
