c6f9dd77ecde2950b791ea3e02a88c79d9351120
Lars Kanis <kanis@comcard.de>
2018-09-28 09:04:27 +0200

    Split the lingering threads check out of the test class
    This is in preparation for splitting the tests into several files.

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,6 +1,28 @@
 require 'simplecov'
 SimpleCov.start
 
+
+BEGIN {
+  @start_threads = Thread.list
+}
+
+END {
+  # Trigger ObjectRegistry#untag and thread stopping
+  GC.start
+  sleep 0.1 if (Thread.list - @start_threads).any?
+
+  lingering = Thread.list - @start_threads
+  if lingering.any?
+    warn "Warning: #{lingering.length} lingering threads"
+    lingering.each do |th|
+      line = th.backtrace&.find{|t| t=~/test\// } or
+          th.backtrace&.find{|t| !(t=~/lib\/eventbox(\/|\.rb:)/) } or
+          th.backtrace&.first
+      warn "    #{ line }"
+    end
+  end
+}
+
 $LOAD_PATH.unshift File.expand_path("../../lib", __FILE__)
 require "eventbox"
 require "minitest/autorun"
