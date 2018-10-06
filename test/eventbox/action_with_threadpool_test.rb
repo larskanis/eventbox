e92b551911ec50fa05c9b0144f610053cbb45eee
Lars Kanis <lars@greiz-reinsdorf.de>
2018-10-06 15:16:03 +0200

    Use more threads for the ThreadPool on JRuby

--- test/eventbox/action_with_threadpool_test.rb
+++ test/eventbox/action_with_threadpool_test.rb
@@ -1,7 +1,9 @@
 require_relative "../test_helper"
 
 class EventboxActionWithThreadpoolTest < Minitest::Test
-  Eventbox = ::Eventbox.with_options(threadpool: Eventbox::ThreadPool.new(3, run_gc_when_busy: true))
+  # JRuby doesn't GC the threads reliably, so that more threads are needed
+  num_threads = RUBY_ENGINE == "jruby" ? 120 : 4
+  Eventbox = ::Eventbox.with_options(threadpool: Eventbox::ThreadPool.new(num_threads, run_gc_when_busy: true))
 
   eval_file "eventbox/action_test_collection.rb"
 
