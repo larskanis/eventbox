b18a3cf8c25b4f494c43879062b0df3b4db11841
Lars Kanis <lars@greiz-reinsdorf.de>
2018-10-24 22:01:34 +0200

    Add an example for parallel http downloads

---
+++ test/examples/downloads_test.rb
@@ -0,0 +1,53 @@
+require "net/https"
+require "open-uri"
+# require "pp"
+require_relative "../test_helper"
+
+class ExamplesDownloadsTest < Minitest::Test
+  class ParallelDownloads < Eventbox.with_options(threadpool: Eventbox::ThreadPool.new(2))
+    yield_call def init(urls, result)
+      @urls =urls
+      @urls.each do |url|
+        start_download(url)
+      end
+      @downloads = {}
+      @finished = result
+    end
+
+    private action def start_download(url)
+      data = OpenURI.open_uri(url).read(100).each_line.first
+    rescue SocketError => err
+      download_finished(url, err)
+    else
+      download_finished(url, data)
+    end
+
+    private sync_call def download_finished(url, res)
+      @downloads[url] = res
+      @finished.yield if @downloads.size == @urls.size
+    end
+
+    attr_reader :downloads
+  end
+
+  def test_queue
+    urls = %w[
+      http://ruby-lang.org
+      http://aruby-lang.ooorg
+      http://wikipedia.org
+      http://torproject.org
+      http://github.com
+      http://google.com
+      http://leo.org
+    ]
+
+    d = ParallelDownloads.new(urls)
+#     pp d.downloads
+
+    dls = d.downloads.sort_by{|k,v| k }
+    assert_equal urls.sort, dls.map(&:first)
+    assert_kind_of Exception, dls[0][1]
+    assert_kind_of String, dls[1][1]
+    assert_kind_of String, dls[2][1]
+  end
+end
