ddf739e9cc059e0d843ce1a485da5884161e5aab
Lars Kanis <lars@greiz-reinsdorf.de>
2019-01-04 18:04:16 +0100

    Fix wrong name of test class

--- test/eventbox/thread_pool_test.rb
+++ test/eventbox/thread_pool_test.rb
@@ -1,6 +1,6 @@
 require_relative "../test_helper"
 
-class EventboxThreadBoxTest < Minitest::Test
+class EventboxThreadPoolTest < Minitest::Test
   def test_number_of_threads
     tp = Eventbox::ThreadPool.new(3)
 
